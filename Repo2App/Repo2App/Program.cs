0badb3f999d7ba327ceb2510fbbb7af31ca69380
Olivér Szabó <oliver.szabo@semilab.hu>
2018-04-24 16:16:43 +0200

    RandomFunctionBox.Func01() added

---
+++ Repo2App/Repo2App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+
+    class RandomFunctionBox1
+    {
+        public static void Func01()
+        {
+            System.Console.WriteLine("Func01 Running");
+        }
+    }
+}
